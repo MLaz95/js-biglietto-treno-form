6e9c3378404fb6fa7ef1837e9016a9c0171a474f
marco <lazzari.marco.ml@gmail.com>
2024-01-30 17:33:09 +0100

    aggiunto stile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // take user info
 const kmInput = document.querySelector("#distance");
 const ageInput = document.querySelector("#age");
-const buttonElement = document.querySelector("#submit");
+const buttonElement = document.querySelector("#confirm");
 
 // button click triggers the funcion
 buttonElement.addEventListener('click',
@@ -14,24 +14,22 @@ buttonElement.addEventListener('click',
             document.getElementById("ticket-price").innerHTML = `Cannot calculate fare with current information, please try again.`;
         
         } else {
-        // distance * 0.21 = price
-        let price = kmInput.value * 0.21
-
-        // if age < 18 then price - 20%
-        if (ageInput.value < 18){
-        price *= 1 - 0.2;
-        console.log(price);
-    
-        // if age > 65 then price - 40%
-        } else if (ageInput.value > 65){
-        price *= 1 - 0.4;
-        console.log(price);
-        }
-
-        // print info on screen
-        document.getElementById("ticket-price").innerHTML = `Ticket Cost ${price.toFixed(2)}€.`;
-        }
-
+            // distance * 0.21 = price
+            let price = kmInput.value * 0.21
+            // if age < 18 then price - 20%
+            if (ageInput.value < 18){
+            price *= 1 - 0.2;
+            document.querySelector("#discount").innerHTML = `The Junior discount has been applied.`
         
+            // if age > 65 then price - 40%
+            } else if (ageInput.value > 65){
+            price *= 1 - 0.4;
+            document.querySelector("#discount").innerHTML = `The Senior discount has been applied.`
+            }
+
+            // print info on screen
+            document.getElementById("ticket-info").innerHTML = `Seat XX, Carriage N`;
+            document.getElementById("ticket-price").innerHTML = `Ticket Cost ${price.toFixed(2)}€.`;
+        }
     }
 )
