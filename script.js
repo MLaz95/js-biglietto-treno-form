8f5fb1fd593a8799f1f39e4d9959745d063b2823
marco <lazzari.marco.ml@gmail.com>
2024-01-30 16:32:22 +0100

    aggiunti check sulle user info

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const kmInput = document.querySelector("#distance");
 const ageInput = document.querySelector("#age");
 const buttonElement = document.querySelector("#submit");
 
-
 // button click triggers the funcion
 buttonElement.addEventListener('click',
     function(){
+        // checks km input is possible
+        if(kmInput.value < 0){
+            document.getElementById("ticket-price").innerHTML = `Cannot calculate fare with current information, please try again.`;
+        // checks age input is possible
+        } else if(ageInput.value < 0 || ageInput.value > 122){
+            document.getElementById("ticket-price").innerHTML = `Cannot calculate fare with current information, please try again.`;
+        
+        } else {
         // distance * 0.21 = price
         let price = kmInput.value * 0.21
 
@@ -23,29 +30,8 @@ buttonElement.addEventListener('click',
 
         // print info on screen
         document.getElementById("ticket-price").innerHTML = `Ticket Cost ${price.toFixed(2)}€.`;
+        }
+
+        
     }
 )
-
-
-// checks if the user's inputs are numbers
-// if(Number.isNaN(km) || Number.isNaN(age)){
-//     document.getElementById("ticket-price").innerHTML = "Non posso calcolare il prezzo del tuo biglietto con questi parametri.<br>Ricarica la pagina per riprovare.";
-
-// checks if the user's age is possible
-// } else if (age > 120){
-//     document.getElementById("ticket-price").innerHTML = "La tua età sembra errata, ricarica la pagina per riprovare.";
-
-// } else {
-    // let price = km * 0.21
-
-    // if (age < 18){
-    // price *= 1 - 0.2;
-    // document.getElementById("discount").innerHTML = `Hai ricevuto lo sconto minorenni del 20%`;
-
-    // } else if (age > 65){
-    // price *= 1 - 0.4;
-    // document.getElementById("discount").innerHTML = `Hai ricevuto lo sconto senior del 40%`;
-    // }
-
-//     document.getElementById("ticket-price").innerHTML = `Il prezzo del tuo biglietto è ${price.toFixed(2)}€.`;
-// }
